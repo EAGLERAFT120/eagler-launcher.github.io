aa0905c6e95704463dedba3283eb3eb492022e04
Colbster937 <96893162+colbychittenden@users.noreply.github.com>
2024-05-18 16:26:20 -0500

    Fixed context menu issue

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ window.addEventListener('load', function() {
         document.body.removeChild(loader);
     });
 });
+document.addEventListener('contextmenu', event => event.preventDefault());
 window.gameFrame = document.querySelector('.game-frame');
 function changeVersion(version) {
     var friendlyVersion = version.replace('latest', 'Latest Release (1.8.8)').replace('classic', 'Classic').replace('ms-Classic', 'Classic (Microsoft Recreation)').replace('beta-1.3', 'Beta 1.3').replace('alpha-1.2.6', 'Alpha 1.2.6').replace('dragonx-client', 'DragonX Client');
